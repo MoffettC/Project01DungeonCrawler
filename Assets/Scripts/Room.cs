7f5103a4240783895b7b202e0ccea5135f5df06a
Christopher Moffett <christophmoffett@gmail.com>
2016-02-21 18:02:45 -0800

    Dungeon Crawler
    Tilemapping

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+    public GameObject[,] room;
+    public float tileSize = 1f;
+
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public Room(int x, int y, int width, int height){
+        x1 = x;
+        x2 = x + width;
+        y1 = y;
+        y2 = y + height;
+     }
+
+
+    public void CreateRoom(GameObject prefab)
+    {
+        if (!prefab)
+            Debug.LogWarning("Unable to find TilePrefab in your Resources folder.");
+        GameObject Room = new GameObject();
+        room = new GameObject[x2, y2];
+
+        for (int i = x1; i < x2; i++)
+        {
+            for (int j = y1; j < y2; j++)
+            {
+               // Debug.Log("x: " + i);
+                //Debug.Log("y: " + j);
+                
+                GameObject floorTile = Instantiate(prefab, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity) as GameObject;
+                //wallTile.transform.parent = Room.transform;
+                room[i, j] = floorTile;
+            }
+        }
+    }
+
+
+    public bool Intersect(Room room)
+    {
+        return (x1 <= room.x2 && x2 >= room.x1 &&
+            y1 <= room.y2 && room.y2 >= room.y1);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public GameObject floorPrefab;
+
+
+    public int x1;
+    public int y1;
+    public int x2;
+    public int y2;
+
+    public int mapWidth;
+    public int mapHeight;
+    public float tileSize = 1.28f;
+    public int maxRooms = 1;
+    public int minRoomSize = 3;
+    public int maxRoomSize = 5;
+
+    public GameObject[,] wallTiles;
+    public bool notWalkable = true;
+    public bool[,] walkableArray;
+
+    void Start()
+    {
+        //Creating Floor Tiles
+        if (!wallPrefab)
+            Debug.LogWarning("Unable to find TilePrefab in your Resources folder.");
+
+        GameObject wallTile = new GameObject();
+
+        wallTiles = new GameObject[mapWidth, mapHeight];
+        walkableArray = new bool[mapWidth, mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                {                                 
+                    wallTile = Instantiate(wallPrefab, new Vector3(x * tileSize, y * tileSize, 0), Quaternion.identity) as GameObject;
+                    walkableArray[x, y] = notWalkable;
+                    //floorTile = floorTiles[x, y];
+                    //wallTiles[x, y] = wallTile;
+                    //floorTiles[x, y].transform.parent = floorMap.transform;
+                }
+            }
+        }     
+        PlaceRooms(floorPrefab);
+    }
+
+    public void PlaceRooms(GameObject prefab)
+    {
+        Room[] rooms = new Room[maxRooms];
+        //bool failed = false;
+
+        for (int i = 0; i < maxRooms; i++)
+        {
+            int w = minRoomSize + Random.Range(1, maxRoomSize - minRoomSize);
+            int h = minRoomSize + Random.Range(1, maxRoomSize - minRoomSize);
+            int x = Random.Range(1, mapWidth - w - 1);
+            int y = Random.Range(1, mapHeight - h - 1);
+
+            //Create room
+           Room newRoom = new Room(x, y, w, h);
+           newRoom.CreateRoom(prefab);
+           rooms[i] = newRoom;
+
+            for (int j = x; j < x + w; j++){
+                for (int k = y; k < y + h; k++) {
+                    {
+                        if (IsWalkable(j, k)) {
+                            walkableArray[j, k] = false;
+                            Debug.Log("Walkable at " + j + k);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public bool IsWalkable(int x, int y)
+    {
+        if (x1 < 0 || y1 < 0 || x > mapWidth || y1 > mapHeight){
+            if (walkableArray[x, y] = notWalkable){
+                return false;
+            }
+            else{
+                return true;
+            }
+        } else{
+            Debug.Log("Coordinates out of bounds");
+            return false;
+        }
+    }
+
+}
