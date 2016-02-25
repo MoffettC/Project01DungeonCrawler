68d0499db7824f8dde8c02a90dcda9b84749ec97
Christopher Moffett <20ninjas@gmail.com>
2016-02-25 13:12:58 -0800

    v0.2

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,13 +3,66 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+    Vector3 pos;
+
+    public int moveSpeed = 4;
+    private TileMap tileMap;
+
 	// Use this for initialization
 	void Start () {
-	
+        pos = transform.position;
+        tileMap = GameObject.FindObjectOfType<TileMap>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.W) && transform.position == pos) {
+            Vector3 playerPos = pos;
+            int playerPosX = (int)(pos.x + 0.5);
+            int playerPosY = (int)(pos.y + 0.5);
+            if (tileMap.Walkable(playerPosX, playerPosY + 1)) {
+                pos += Vector3.up;
+            } else {
+                Debug.Log("Cant Walk Up!");
+            }
+
+            //transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A) && transform.position == pos) {
+            Vector3 playerPos = pos;
+            int playerPosX = (int)(pos.x + 0.5);
+            int playerPosY = (int)(pos.y + 0.5);
+            if (tileMap.Walkable(playerPosX - 1, playerPosY)) {
+                pos += Vector3.left;
+            } else {
+                Debug.Log("Cant Walk Left!");
+            }
+
+            // transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S) && transform.position == pos) {
+            Vector3 playerPos = pos;
+            int playerPosX = (int)(pos.x + 0.5);
+            int playerPosY = (int)(pos.y + 0.5);
+            if (tileMap.Walkable(playerPosX, playerPosY - 1)) {
+                pos += Vector3.down;
+            } else {
+                Debug.Log("Cant Walk Down!");
+            }
+            // transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && transform.position == pos) {
+            Vector3 playerPos = pos;
+            int playerPosX = (int)(pos.x + 0.5);
+            int playerPosY = (int)(pos.y + 0.5);
+            if (tileMap.Walkable(playerPosX + 1, playerPosY)) {
+                pos += Vector3.right;
+            } else {
+                Debug.Log("Cant Walk Right!");
+            }
+            // transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * moveSpeed);
+    }
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,40 +4,50 @@ using System.Collections;
 public class Room : MonoBehaviour {
 
     public GameObject[,] room;
+    public GameObject character;
     public float tileSize = 1f;
 
     private int x1;
     private int y1;
     private int x2;
     private int y2;
+    public Vector2 center;
 
     public Room(int x, int y, int width, int height){
         x1 = x;
         x2 = x + width;
         y1 = y;
         y2 = y + height;
+        center = new Vector2(((x1 + x2) / 2), ((y1 + y2) / 2));
      }
 
 
-    public void CreateRoom(GameObject prefab)
+    public GameObject CreateRoom(GameObject prefab)
     {
         if (!prefab)
             Debug.LogWarning("Unable to find TilePrefab in your Resources folder.");
+
         GameObject Room = new GameObject();
+        Room.name = "Room";
         room = new GameObject[x2, y2];
 
-        for (int i = x1; i < x2; i++)
-        {
-            for (int j = y1; j < y2; j++)
-            {
-               // Debug.Log("x: " + i);
-                //Debug.Log("y: " + j);
-                
+        for (int i = x1; i < x2; i++){
+            for (int j = y1; j < y2; j++){             
                 GameObject floorTile = Instantiate(prefab, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity) as GameObject;
-                //wallTile.transform.parent = Room.transform;
+                floorTile.transform.parent = Room.transform;
+                floorTile.name = "floorTile" + i + j;
                 room[i, j] = floorTile;
+                RemoveWallTiles(i, j);
+                //GameObject playerChar = Instantiate(character, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity) as GameObject;
             }
         }
+        return Room;
+    }
+
+    //Need to actually remove tiles and not use value as moveable/nonmoveable
+    public void RemoveWallTiles(int i, int j) {
+        TileMap.wallTiles[i, j] = 0;
+        Debug.Log(TileMap.wallTiles[i, j]);
     }
 
 
